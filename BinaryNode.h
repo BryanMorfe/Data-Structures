82967b17cdda3767dce77375eb17390b9da6e6f0
BryanMorfe <bryanmorfe@gmail.com>
2017-10-28 21:02:09 -0400

    Implemented templated constructors and member functions of BinaryNode object. Comments still needed.

--- BinaryNode.h
+++ BinaryNode.h
@@ -26,4 +26,52 @@ private:
     BinaryNode<T> *leftNode;
     BinaryNode<T> *rightNode;
     T item;
+};
+
+template <class T>
+BinaryNode<T>::BinaryNode(): leftNode(nullptr), rightNode(nullptr), item()
+{}
+
+template <class T>
+BinaryNode<T>::BinaryNode(const T &t): item(t), leftNode(nullptr), rightNode(nullptr)
+{}
+
+template <class T>
+BinaryNode<T>::BinaryNode(const T &t, BinaryNode<T> *ln, BinaryNode *rn): item(t), leftNode(ln), rightNode(rn)
+{}
+
+template <class T>
+void BinaryNode<T>::setItem(const T &t)
+{
+    item = t;
+}
+
+template <class T>
+void BinaryNode<T>::setLeftNode(BinaryNode<T> *ln)
+{
+    leftNode = ln;
+}
+
+template <class T>
+void BinaryNode<T>::setLeftNode(BinaryNode<T> *rn)
+{
+    rightNode = rn;
+}
+
+template <class T>
+const T BinaryNode<T>::getItem()
+{
+    return item;
+}
+
+template <class T>
+BinaryNode<T> *BinaryNode<T>::getLeftNode()
+{
+    return leftNode;
+}
+
+template <class T>
+BinaryNode<T> *BinaryNode<T>::getRightNode()
+{
+    return rightNode;
 }
